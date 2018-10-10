2c4e02a7094cb896360d1d9d69b7073c9058855a
Jason Cook <the.root.user@gmail.com>
2018-10-09 23:00:05 -0400

    Basic rudimentary design, I'm lagging in getting this done :(

---
+++ script.js
@@ -0,0 +1,3 @@
+let anchorInNav = document.getElementsByClassName("nav")
+const tween1 = TweenMax.to(anchorInNav, 2, {textShadow: "1px 1px 10px rgb(180, 180, 180)", color:"#004321", yoyoEase:Strong.easeInOut, repeat: 11});   
+tween1();   
