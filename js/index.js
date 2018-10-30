fb67a2f830ea01ffb6b7e108901a9d9c51a9e2ad
1@$@1github <asaofdiamonds@gmail.com>
2018-10-30 16:14:10 -0600

    added animations to the cta top

---
+++ js/index.js
@@ -0,0 +1,4 @@
+TweenMax.from('.headLine', 5, {scale: 0.2, opacity: 0, color: 'red' });
+TweenMax.from('.asaPhoto', 5, {x: 600,  scale: 0.2, opacity: 0,});
+TweenMax.to('.aceOfDiamonds', 5, { x: 300, scale: 0.75, rotation: 360, skewY: 15});
+TweenMax.to('#gitHub', 5, { color: 'red'})
