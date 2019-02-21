10a9a19da69bc3c1be85179cb6a336834b70eac2
jfkinslow <killajk2201@gmail.com>
2019-02-20 23:35:07 -0600

    Finished project after hours of work and lots of trips to google

---
+++ js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $(".container section.cta div.cta-text.bottom-content button.cta-button").click(function(){
+        window.open("https://goo.gl/forms/HdWQjL7chcpfgsqr1", "_blank");
+    });
+});
