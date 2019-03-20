42a4096e307ae06246f99f050118e0cd3b5f867f
myasia barbee <myasiabarbee@icloud.com>
2019-03-19 22:48:12 -0400

    JS code added for navbar

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
