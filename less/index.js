882d95ac82d67b577a8abb0d26b0fe5c99e8388a
Timothy Ross Grey <TimGrey@Timothys-MacBook-Pro-7.local>
2019-08-28 18:29:03 -0700

    Added slide show.Added About page, Contact page which all link to home page. Added email form to contact page for questions / contact information. Finished main format, color, spacing and styling.

---
+++ less/index.js
@@ -0,0 +1,20 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 9000);    
+}
+
+// ===================================
+
+function myFunction() {
+  alert("Your mail has been sent!");
+}
