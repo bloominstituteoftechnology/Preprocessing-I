4404f2084856a779c0e79b5c9ae3706f86610a75
Danika Thomson <danikathomson@macusers-iMac.local>
2019-10-10 00:14:00 +0200

    added mousehover event to buttons, added box shadow

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const buttonOne = document.querySelector("#btn-1");
+const skillsBoxOne = document.querySelector("#skills-box-1")
+console.log(buttonOne);
+console.log(skillsBoxOne);
+
+buttonOne.addEventListener('mouseover', (callback) => { 
+    event.target.style.backgroundColor = "grey"});;
+
+console.log("Helloooooo");
