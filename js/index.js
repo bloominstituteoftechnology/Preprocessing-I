4c9c2b244333b3842f3cc40dc82519e4dd82deb2
Danika Thomson <danikathomson@macusers-iMac.local>
2019-10-10 23:19:44 +0200

    Added media query @mobile 500px

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ const skillsBoxOne = document.querySelector("#skills-box-1")
 console.log(buttonOne);
 console.log(skillsBoxOne);
 
+
+
 buttonOne.addEventListener('mouseover', (callback) => { 
     event.target.style.backgroundColor = "grey"});;
 
