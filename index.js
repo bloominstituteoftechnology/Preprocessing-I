6c6289ebe129fdfa5392a0b77c450c3e85691b5f
jacob <jacobcunningham96@yahoo.com>
2020-03-03 17:22:31 -0700

    added some css

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const list = document.querySelector('ul');
 const form = document.querySelector('form');
+const remove = document.querySelector('.remove');
 const addThing = (things, id) => {
-    let time = things.time.toDate();
      let html = `
      <li data-id="${id}">
      <div>${things.title}</div>
-     <div><i>${time}</i></div>
      </li>
      `;
   list.innerHTML += html;   
      }
-
+    //  const deleteRecipe = id => {
+    //     const recipes = document.querySelectorAll('li');
+    //     recipes.forEach(recipe => {
+    //         if(thing.getAttribute('data-id') === id){
+    //             recipe.remove();
+    //         }
+    //     });
+    //  }
      db.collection('thing I can do').onSnapshot(snapshot => {
         snapshot.docChanges().forEach(change => {
         const doc = change.doc;
         if(change.type === 'added'){
             addThing(doc.data(), doc.id);
-        } 
-    });
+        // } else if (change.type === 'removed'){
+        //     deleteRecipe(doc.id);
+        }
+    
    });
         form.addEventListener('submit', e => {
         e.preventDefault();
@@ -26,10 +34,31 @@ const addThing = (things, id) => {
             title: form.thing.value,
             time: firebase.firestore.Timestamp.fromDate(now),
         }
-        
+    
         db.collection('thing I can do').add(thing).then(() => {
             console.log('thing added');
         }).catch(err => {
             console.log(err);
         });
         });
+    list.addEventListener('click', e => {
+        if(e.target.tagName === "LI"){
+                    const id = e.target.parentElement.getAttribute('data-id');
+                    db.collection('thing I can do').doc(id).delete().then(() => {
+                        console.log(`thing has been deleted`);
+                    });
+                    }
+                    });
+    //     list.addEventListener('click', e => {
+    //         if(e.target.tagName === remove){
+    //         const id = e.target.parentElement.getAttribute('data-id');
+    //         db.collection('thing I can do').doc(id).delete().then(() => {
+    //             console.log(`thing has been deleted`);
+    //         });
+    //         }
+    //         });
+     });
+const button = document.querySelector('.button');
+button.addEventListener('click', e => {
+    alert(`Nah, you're not cool enough. Try again.`)
+});
