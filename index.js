33d7b48a8a2a140e0915c21c0b917f6880e5aba3
glassbones <glassbonesofficial@gmail.com>
2020-04-14 09:48:54 -0500

    assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+/*$(function() {
+
+    var $body = $(document);
+    $body.bind('scroll', function() {
+        // "Disable" the horizontal scroll.
+        if ($body.scrollLeft() !== 0) {
+            $body.scrollLeft(0);
+        }
+    });
+
+});*/
+//  <script src="index.js"></script> 
